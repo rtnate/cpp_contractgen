39d6037227bf5fdb7acf3f83c5122cd4bad9f6f6
Nate Taylor <nate@rtelectronix.com>
2025-09-26 12:01:50 -0500

    Refactor: removed error handling from __main__.py
    Moved error handling to cli.run_cli to keep main lean and handle exceptions
    and errors in the cli and return exit codes and values where they belong

--- src/cpp_contractgen/__main__.py
+++ src/cpp_contractgen/__main__.py
@@ -5,27 +5,28 @@ import logging
 from .errors import ArgParseError, ConfigError, ParseError
 
 if __name__ == "__main__":
-    try:
-        exit_code = run_cli()
-        sys.exit(exit_code)
-    except ArgParseError as e:
-        logging.error("Argument error: %s", e)
-        sys.exit(ExitCode.BAD_ARGS)
-    except ConfigError as e:
-        logging.error("Configuration error: %s", e)
-        sys.exit(ExitCode.CONFIG_ERROR)
-    except ParseError as e:
-        logging.error("Parse error: %s", e)
-        sys.exit(ExitCode.PARSE_ERROR)
-    except FileExistsError as e:
-        logging.error("File Exists error: %s", e)
-        sys.exit(ExitCode.FILE_EXISTS_ERROR)
-    except Exception as e:
-        # Only show stack trace if verbose mode is on
-        log = logging.getLogger()
-        logLevel = log.getEffectiveLevel()
-        if logLevel <= logging.DEBUG:
-            logging.critical("Unexpected error", exc_info=True)
-        else:
-            logging.critical("Unexpected error: %s", e)
-        sys.exit(ExitCode.INTERNAL_ERROR)
+    run_cli()
+    # try:
+    #     exit_code = run_cli()
+    #     sys.exit(exit_code)
+    # except ArgParseError as e:
+    #     logging.error("Argument error: %s", e)
+    #     sys.exit(ExitCode.BAD_ARGS)
+    # except ConfigError as e:
+    #     logging.error("Configuration error: %s", e)
+    #     sys.exit(ExitCode.CONFIG_ERROR)
+    # except ParseError as e:
+    #     logging.error("Parse error: %s", e)
+    #     sys.exit(ExitCode.PARSE_ERROR)
+    # except FileExistsError as e:
+    #     logging.error("File Exists error: %s", e)
+    #     sys.exit(ExitCode.FILE_EXISTS_ERROR)
+    # except Exception as e:
+    #     # Only show stack trace if verbose mode is on
+    #     log = logging.getLogger()
+    #     logLevel = log.getEffectiveLevel()
+    #     if logLevel <= logging.DEBUG:
+    #         logging.critical("Unexpected error", exc_info=True)
+    #     else:
+    #         logging.critical("Unexpected error: %s", e)
+    #     sys.exit(ExitCode.INTERNAL_ERROR)
